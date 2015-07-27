342d0b98c5eeb484bb98703a38f47005c81d517c
Runar Bjarnason <runar.bjarnason@oncue.com>
2015-07-27 16:39:57 -0700

    ..

--- src/main/scala/compiler/codegen/GenCode.scala
+++ src/main/scala/compiler/codegen/GenCode.scala
@@ -1,21 +1,27 @@
 package kex
 package compiler
 package codegen
+import Types._
+import kex.compiler.{ProtocolTypes => PT}
+import scalaz.std.list._
+import scalaz.std.option._
+import scalaz.syntax.traverse._
 
-trait GenCode[A] {
+trait GenCode {
   type Container
+  type AST
 
   def generateContainer(bs: Bindings, decl: Declaration): Option[Container]
   def msgDeclGenerators: Map[String, MsgDeclGenerator[Container]]
   def typeDeclGenerators: Map[String, TypeDeclGenerator[Container]]
-  def generateCode(containers: List[Container]): A
+  def generateCode(containers: List[Container]): AST
 }
 
 trait MsgDeclGenerator[C] {
   def apply(bindings: Bindings,
             name: String,
-            expr: MessageDef,
-            opts: TypeOptions,
+            expr: PT.MessageDef,
+            opts: PT.TypeOptions,
             cont: C): C
 }
 
@@ -23,14 +29,16 @@ trait TypeDeclGenerator[C] {
   def apply(bindings: Bindings,
             name: String,
             params: List[String],
-            expr: Type,
-            opts: TypeOptions,
+            expr: PT.Type,
+            opts: PT.TypeOptions,
             cont: C): C
 }
 
 object GenScala extends MacrosCompatibility {
 
-  class Generator(ctx: Context) extends GenCode[ctx.Tree] {
+  case class Generator(ctx: Context) extends GenCode {
+
+    import ctx.universe.{Constant => _, _}
 
     type AST = ctx.Tree
 
@@ -51,18 +59,18 @@ object GenScala extends MacrosCompatibility {
       ScalaContainer(name, None, Some(types), None, None, None, None, func)
 
     def defaultValue(v: LowLevel): Option[AST] = v match {
-      case Vint(VBool) => Some(q"false")
+      case Vint(VBool, _) => Some(q"false")
       case Sum(xs, _) => xs.flatMap {
-        case Constant(x) => Some(q"${ctx.parse(x.typ.capitalize)}.${ctx.parse(x.name)}")
+        case Constant(x) => Some(q"${ctx.parse(x.tpe.capitalize)}.${TermName(x.name)}")
         case _ => None
-      }
+      }.headOption
       case Htuple(HList, _, _) => Some(q"List()")
       case Htuple(HArray, _, _) => Some(q"Array()")
       case LLMessage(path, name, _) =>
         val fullPath = path ++ List(name.capitalize)
         val id = identWithPath(ctx)(fullPath, s"${name}_default")
         Some(q"$id")
-      case Tuple(tys) => tys.traverse(defaultValue).map {
+      case Tuple(tys, _) => tys.traverse(defaultValue).map {
         case Nil      => sys.error("defaultValue: empty tuple")
         case _ :: Nil => sys.error("defaultValue: tuple with only 1 element")
         case x :: xs  => q"($x, ..$xs)"
@@ -70,7 +78,7 @@ object GenScala extends MacrosCompatibility {
       case _ => None
     }
 
-    def lookupOption(name: String, opts: TypeOptions, global: Boolean = false): Option[String] =
+    def lookupOption(name: String, opts: PT.TypeOptions, global: Boolean = false): Option[String] =
       if (global) opts.get(name) else opts.get(s"scala.$name")
 
     def parseType(ty: String) = {
@@ -79,127 +87,136 @@ object GenScala extends MacrosCompatibility {
     }
 
     def parseExpr(exp: String) = {
-      val q"val x = $e" = ctx.parse(s"val x = $expr")
+      val q"val x = $e" = ctx.parse(s"val x = $exp")
       e
     }
 
     import java.util.regex.Pattern.quote
 
-    def getTypeInfo(opts: TypeOptions): Option[(ctx.Tree, ctx.Tree, ctx.Tree)] =
-      lookupOption("type", opts).map {
+    def getTypeInfo(opts: PT.TypeOptions): Option[(ctx.Tree, ctx.Tree, ctx.Tree)] =
+      lookupOption("type", opts).flatMap {
         v => v.split(",").map(_.trim) match {
-          case List(ty, from, to) =>
+          case Array(ty, from, to) =>
             Some((parseType(ty), parseExpr(from), parseExpr(to)))
           case _ => None
         }
       }
 
-    def getType(default: ctx.Tree, opts: TypeOptions): ctx.Tree =
+    def getType(default: ctx.Tree, opts: PT.TypeOptions): ctx.Tree =
       getTypeInfo(opts).map(_._1).getOrElse(default)
 
     def generateContainer(bindings: Bindings): ScalaContainer = {
-      def typeDecl(name: String, ctyp: ctx.Tree, params: List[String] = List()) =
-
+      def typeDecl(name: String, ctyp: ctx.Tree, params: List[String] = List()): ctx.Tree = {
+        val t = TypeName(name)
+        val ps = params.map { p =>
+          val q"type dummy[$x] = Int" = q"type dummy[${TypeName(p)}] = Int"
+          x
+        }
+        q"type $t[..$ps] = $ctyp"
+      }
+      ???
     }
   }
 }
 
 case class BadOption(msg: String) extends Exception
 
-object GenCode {
-
-  import Types._
-  val P = ProtocolTypes
-
-  def generateScala(c: Context)(decls: List[Declaration]) = {
-    val bindings = collectBindings(decls)
-    decls.flatMap { decl =>
-      generateContainer(bindings).map { cont =>
-        decl match {
-          case TypeDecl(name, params, expr, opts) =>
-            typeDeclGenerators.foldLeft
-        }
-      }
-    }
-  }
-
-  /*def updateBindings(bindings: Bindings,
-                     params: List[String],
-                     args: List[LowLevel]): Bindings =
-    (params, args).zipped.foldLeft(bindings)(_ + _)
-  */
-  /*
-
-  def betaReduceTexpr(bindings: Bindings, texpr: P.BaseType): ReducedType = {
-    def aux(bindings: Bindings, x: P.BaseType): ReducedType =
-    betaReduceTexprAux(aux, betaReduceTexpr, bindings, texpr)
-  }
-
-  def betaReduceTexprAux(f: (Bindings, P.BaseType) => ReducedType,
-                         self: Bindings => P.BaseType => ReducedType,
-                         bindings: Bindings,
-                         expr: P.BaseType): ReducedType = expr match {
-    case CoreT(x@SimpleT(e)) =>
-    case CoreT(TupleT(l, opts)) => CoreT(TupleT(l.map(self(bindings)), opts))
-    case CoreT(ListT(t, opts)) => CoreT(ListT(self(bindings)(t), opts))
-    case CoreT(ArrayT(t, opts)) => CoreT(ArrayT(self(bindings)(t), opts))
-    case x => f(bindings, x)
-  }
-
-  def betaReduceSum(self: Bindings => P.BaseType => ReducedType,
-                    bindings: Bindings,
-                    s: SumDataType[P.BaseType],
-                    opts: P.TypeOptions): ReducedType = {
-    val constructors = s.constructors.map {
-      case x@compiler.Constant(_) => x
-      case compiler.NonConstant(const, tys) =>
-        compiler.NonConstant(const, tys.map(self(bindings)))
-    }
-    SumRT(s copy (constructors = constructors), opts)
-  }
-
-  def betaReduceRecord(self: Bindings => P.BaseType => ReducedType,
-                       bindings: Bindings,
-                       r: RecordDataType[P.BaseType],
-                       opts: P.TypeOptions) = {
-    val fields = r.fields.map {
-      case Field(name, mutable, ty) => Field(name, mutable, self(bindings)(ty))
-    }
-    RecordRT(r copy (fields = fields), opts)
-  }
-
-  def betaReduceTexpr(bindings: Bindings, texpr: P.BaseType): ReducedType = {
-    def aux(bindings: Bindings, x: TypeExpr): ReducedType = {
-      def reduceTexpr(bindings: Bindings, expr: TypeExpr): P.BaseType = expr match {
-        case BaseT(e) => betaReduceTexpr(bindings, e)
-        case x => aux(bindings, x)
-      }
-      x match {
-        case SumT(s, opts) => right(betaReduceSum(betaReduceTexpr, bindings, s, opts))
-        case RecordT(r, opts) => right(betaReduceRecord(betaReduceTexpr, bindings, r, opts))
-        case BaseT(TypeParamT(p)) =>
-          val name = p.toString
-          (bindings get name) match {
-            case Some(MessageDecl(name, _, opts)) => right(MessageRT(name, opts))
-            case Some(TypeDecl(_, Nil, exp, opts)) =>
-              right(reduceTexpr(bindings, exp) mergeOptions opts)
-            case Some(TypeDecl(_, _, _, _)) =>
-              left(s"Wrong arity for higher-order type $name.")
-            case None => left(s"Unbound type variable $name.")
-          }
-        case BaseT(ExtAppT(path, name, Nil, opts)) => right(ExtMessageRT(path, name, opts))
-        case BaseT(ExtAppT(_, _, _, _)) => left("External polymorphic types not supported")
-        case BaseT(AppT(name, args, opts)) => bindings.get(name) match {
-          case Some(MessageDecl(_, _, opts2)) => right(MessageRT(name, opts2 ++ opts))
-          case Some(TypeDecl(_, params, exp, opts2)) =>
-            val os = opts2 ++ opts
-            val bs = updateBindings(bindings,
-                                    params map (Params.toString(_)),
-                                    args map (ty => TypeDecl("bogus", Nil, ty.tpe)))
-            val b = ???
-            ???
-        }
-      }
-    }
-  }*/
-}
+// object GenCode {
+//
+//   import Types._
+//   val P = ProtocolTypes
+//
+//   def generateScala(c: Context)(decls: List[Declaration]) = {
+//     val G = new GenScala.Generator(c)
+//     import G._
+//     val bindings = collectBindings(decls)
+//     decls.flatMap { decl =>
+//       generateContainer(bindings).map { cont =>
+//         decl match {
+//           case TypeDecl(name, params, expr, opts) => ???
+//             //typeDeclGenerators.foldLeft
+//         }
+//       }
+//     }
+//   }
+//
+//   /*def updateBindings(bindings: Bindings,
+//                      params: List[String],
+//                      args: List[LowLevel]): Bindings =
+//     (params, args).zipped.foldLeft(bindings)(_ + _)
+//   */
+//   /*
+//
+//   def betaReduceTexpr(bindings: Bindings, texpr: P.BaseType): ReducedType = {
+//     def aux(bindings: Bindings, x: P.BaseType): ReducedType =
+//     betaReduceTexprAux(aux, betaReduceTexpr, bindings, texpr)
+//   }
+//
+//   def betaReduceTexprAux(f: (Bindings, P.BaseType) => ReducedType,
+//                          self: Bindings => P.BaseType => ReducedType,
+//                          bindings: Bindings,
+//                          expr: P.BaseType): ReducedType = expr match {
+//     case CoreT(x@SimpleT(e)) =>
+//     case CoreT(TupleT(l, opts)) => CoreT(TupleT(l.map(self(bindings)), opts))
+//     case CoreT(ListT(t, opts)) => CoreT(ListT(self(bindings)(t), opts))
+//     case CoreT(ArrayT(t, opts)) => CoreT(ArrayT(self(bindings)(t), opts))
+//     case x => f(bindings, x)
+//   }
+//
+//   def betaReduceSum(self: Bindings => P.BaseType => ReducedType,
+//                     bindings: Bindings,
+//                     s: SumDataType[P.BaseType],
+//                     opts: P.TypeOptions): ReducedType = {
+//     val constructors = s.constructors.map {
+//       case x@compiler.Constant(_) => x
+//       case compiler.NonConstant(const, tys) =>
+//         compiler.NonConstant(const, tys.map(self(bindings)))
+//     }
+//     SumRT(s copy (constructors = constructors), opts)
+//   }
+//
+//   def betaReduceRecord(self: Bindings => P.BaseType => ReducedType,
+//                        bindings: Bindings,
+//                        r: RecordDataType[P.BaseType],
+//                        opts: P.TypeOptions) = {
+//     val fields = r.fields.map {
+//       case Field(name, mutable, ty) => Field(name, mutable, self(bindings)(ty))
+//     }
+//     RecordRT(r copy (fields = fields), opts)
+//   }
+//
+//   def betaReduceTexpr(bindings: Bindings, texpr: P.BaseType): ReducedType = {
+//     def aux(bindings: Bindings, x: TypeExpr): ReducedType = {
+//       def reduceTexpr(bindings: Bindings, expr: TypeExpr): P.BaseType = expr match {
+//         case BaseT(e) => betaReduceTexpr(bindings, e)
+//         case x => aux(bindings, x)
+//       }
+//       x match {
+//         case SumT(s, opts) => right(betaReduceSum(betaReduceTexpr, bindings, s, opts))
+//         case RecordT(r, opts) => right(betaReduceRecord(betaReduceTexpr, bindings, r, opts))
+//         case BaseT(TypeParamT(p)) =>
+//           val name = p.toString
+//           (bindings get name) match {
+//             case Some(MessageDecl(name, _, opts)) => right(MessageRT(name, opts))
+//             case Some(TypeDecl(_, Nil, exp, opts)) =>
+//               right(reduceTexpr(bindings, exp) mergeOptions opts)
+//             case Some(TypeDecl(_, _, _, _)) =>
+//               left(s"Wrong arity for higher-order type $name.")
+//             case None => left(s"Unbound type variable $name.")
+//           }
+//         case BaseT(ExtAppT(path, name, Nil, opts)) => right(ExtMessageRT(path, name, opts))
+//         case BaseT(ExtAppT(_, _, _, _)) => left("External polymorphic types not supported")
+//         case BaseT(AppT(name, args, opts)) => bindings.get(name) match {
+//           case Some(MessageDecl(_, _, opts2)) => right(MessageRT(name, opts2 ++ opts))
+//           case Some(TypeDecl(_, params, exp, opts2)) =>
+//             val os = opts2 ++ opts
+//             val bs = updateBindings(bindings,
+//                                     params map (Params.toString(_)),
+//                                     args map (ty => TypeDecl("bogus", Nil, ty.tpe)))
+//             val b = ???
+//             ???
+//         }
+//       }
+//     }
+//   }*/
+// }
